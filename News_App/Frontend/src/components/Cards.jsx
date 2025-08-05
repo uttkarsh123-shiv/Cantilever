26557194559dd513d955b724b3c5d5b2017da44c
uttkarsh singh <uttkarshsingh450@gmail.com>
2025-08-05 16:44:43 +0530

    frontend-tweaks

--- News_App/Frontend/src/components/Cards.jsx
+++ News_App/Frontend/src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Cards = () => {
+    const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
   return (
     <div>
       
--- News_App/Frontend/src/pages/Login.jsx
+++ News_App/Frontend/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", { email, password });
+      const res = await axios.post("http://localhost:3000/api/users/login", { email, password });
       alert("Login successful");
       console.log(res.data);
     } catch (err) {
